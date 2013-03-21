bb2a990ff5c4738d44c4b9f7d8905404b88b1857
xzdandy <xzdandy@gmail.com>
2013-03-21 00:28:15 -0700

    initiate the project

---
+++ src/header/header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H_ 
+#define HEADER_H_ 
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <wait.h> 
+#include <signal.h> 
+#include <sstream> 
+#include <pthread.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#include <string.h> 
+#include <ctype.h> 
+#include <sys/time.h> 
+#endif  /HEADER_H_*/
---
+++ src/problem1.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  problem1.c
+ *
+ *    Description:  copy file
+ *
+ *        Version:  1.0
+ *        Created:  03/19/2013 05:56:05 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  xzdandy, 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "header/header.h"
+
+void main(int argc, char* argv[]){
+    
+}
